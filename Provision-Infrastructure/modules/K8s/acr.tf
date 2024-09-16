3ff7a226ebaed5edb7a127fbeeefeec122c5e71a
Nadav Salman <nadavops@gmail.com>
2024-09-16 22:32:42 +0300

    Update flow

--- Provision-Infrastructure/modules/K8s/acr.tf
+++ Provision-Infrastructure/modules/K8s/acr.tf
@@ -1,6 +1,7 @@
 locals {
   acr_name = "${replace(var.dns_prefix, "-", "")}${replace(random_pet.rg.id, "-", "")}acr"
 }
+
 resource "azurerm_container_registry" "default" {
   name                = local.acr_name
   resource_group_name = azurerm_resource_group.k8s_rg.name
