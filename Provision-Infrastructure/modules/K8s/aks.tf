df7f43faa915b103615a8f5ecd25a7952f28a39c
Nadav Salman <nadavops@gmail.com>
2024-09-16 22:05:59 +0300

    Update aks configuration and install ingress contoller vi helm provider

--- Provision-Infrastructure/modules/K8s/aks.tf
+++ Provision-Infrastructure/modules/K8s/aks.tf
@@ -4,13 +4,18 @@ resource "azurerm_kubernetes_cluster" "default" {
   resource_group_name               = azurerm_resource_group.k8s_rg.name
   dns_prefix                        = "${var.dns_prefix}-${var.name}-aks-${var.environment}"
   role_based_access_control_enabled = true
+  oidc_issuer_enabled               = true
+  workload_identity_enabled         = true
 
   default_node_pool {
     name            = "default"
     vm_size         = var.node_type
     node_count      = var.node_count
+    min_count       = 1
+    max_count       = 2
     os_disk_size_gb = 30
   }
+
   identity {
     type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.aks.id]
