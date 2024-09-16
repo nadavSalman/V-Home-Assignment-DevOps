570be20abcddbe9eae60944187158b77bca74ef3
Nadav Salman <nadavops@gmail.com>
2024-09-17 00:39:55 +0300

    Update tf

--- Provision-Infrastructure/modules/K8s/nginx-ingress-controller.tf
+++ Provision-Infrastructure/modules/K8s/nginx-ingress-controller.tf
@@ -1,10 +1,10 @@
-# data "azurerm_kubernetes_cluster" "this" {
-#   name                = azurerm_kubernetes_cluster.default.name
-#   resource_group_name = azurerm_resource_group.k8s_rg.name
+data "azurerm_kubernetes_cluster" "this" {
+  name                = azurerm_kubernetes_cluster.default.name
+  resource_group_name = azurerm_resource_group.k8s_rg.name
 
-#   # Comment this out if you get: Error: Kubernetes cluster unreachable 
-#   depends_on = [azurerm_kubernetes_cluster.default]
-# }
+  # Comment this out if you get: Error: Kubernetes cluster unreachable 
+  # depends_on = [azurerm_kubernetes_cluster.default]
+}
 
 # provider "helm" {
 #   kubernetes {
