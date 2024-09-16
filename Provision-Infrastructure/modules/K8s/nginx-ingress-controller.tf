d1f19d4eb31ab6b9eedda2a2fe38c1640071bf23
Nadav Salman <nadavops@gmail.com>
2024-09-17 00:50:07 +0300

    Update tf

--- Provision-Infrastructure/modules/K8s/nginx-ingress-controller.tf
+++ Provision-Infrastructure/modules/K8s/nginx-ingress-controller.tf
@@ -6,24 +6,24 @@ data "azurerm_kubernetes_cluster" "this" {
   # depends_on = [azurerm_kubernetes_cluster.default]
 }
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.azurerm_kubernetes_cluster.this.kube_config.0.host
-#     client_certificate     = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
-#     client_key             = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_key)
-#     cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.this.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+  }
+}
 
-# resource "helm_release" "external_nginx" {
-#   depends_on = [azurerm_kubernetes_cluster.default]
-#   name       = "external"
+resource "helm_release" "external_nginx" {
+  depends_on = [azurerm_kubernetes_cluster.default]
+  name       = "external"
 
-#   repository       = "https://kubernetes.github.io/ingress-nginx"
-#   chart            = "ingress-nginx"
-#   namespace        = "ingress"
-#   create_namespace = true
-#   version          = "4.8.0"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.8.0"
 
-#   values = [file("${path.module}/nginx-ingress-controller-values/ingress.yaml")]
-# }
+  values = [file("${path.module}/nginx-ingress-controller-values/ingress.yaml")]
+}
