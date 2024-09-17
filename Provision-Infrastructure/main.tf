fa7fc43259a1c21d34d8388fe543c2b80b088c4c
Nadav Salman <nadavops@gmail.com>
2024-09-17 18:57:34 +0300

    update  tf

--- Provision-Infrastructure/main.tf
+++ Provision-Infrastructure/main.tf
@@ -22,12 +22,12 @@ module "K8sInfra" {
 module "DeploymentPrep" {
   source = "./modules/DeploymentPrep"
 
-  subscription_id      = module.K8sInfra.subscription_id
-  aks_name             = module.K8sInfra.aks_name
-  aks_rg_name          = module.K8sInfra.aks_rg_name
-  acr_name             = module.K8sInfra.acr_name
-  storage_account_name = module.K8sInfra.storage_account_name
-  container_name       = module.K8sInfra.storage_container_name
+  subscription_id              = module.K8sInfra.subscription_id
+  aks_name                     = module.K8sInfra.aks_name
+  aks_rg_name                  = module.K8sInfra.aks_rg_name
+  acr_name                     = module.K8sInfra.acr_name
+  storage_account_name         = module.K8sInfra.storage_account_name
+  container_name               = module.K8sInfra.storage_container_name
   storace_accoun_uai_client_id = module.K8sInfra.backend_storage_system_uai_client_id
 }
 
